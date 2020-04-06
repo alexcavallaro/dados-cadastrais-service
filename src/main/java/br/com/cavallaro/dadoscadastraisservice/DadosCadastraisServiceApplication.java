ff4bd01be183bb8f26c512268837886dd293c4ef
Alexander <alexcavallaro@gmail.com>
2020-04-06 20:16:37 -0300

    tests, logs

--- src/main/java/br/com/cavallaro/dadoscadastraisservice/DadosCadastraisServiceApplication.java
+++ src/main/java/br/com/cavallaro/dadoscadastraisservice/DadosCadastraisServiceApplication.java
@@ -2,6 +2,8 @@ package br.com.cavallaro.dadoscadastraisservice;
 
 import java.util.LinkedHashMap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +16,11 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class DadosCadastraisServiceApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(DadosCadastraisServiceApplication.class);
+
 	private DadosCadastrais dadosCadastrais = new DadosCadastrais();
+	
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DadosCadastraisServiceApplication.class, args);
@@ -22,7 +28,8 @@ public class DadosCadastraisServiceApplication {
 
 	@GetMapping("/dados-cadastrais/{cpf}")
 	public Object get(@PathVariable String cpf) {
-		
+		LOGGER.info("/dados-cadastrais/{cpf} "+cpf);
+
 		//getDados(cpf)
 		dadosCadastrais.setCpf(cpf);
 		dadosCadastrais.setEndereco(new Endereco((new RestTemplate()).getForObject(
@@ -33,7 +40,8 @@ public class DadosCadastraisServiceApplication {
 	
 	@PutMapping("/dados-cadastrais/{cpf}/endereco/{cep}")
 	public Object put(@PathVariable String cpf, @PathVariable String cep) {
-		
+		LOGGER.info("/dados-cadastrais/{cpf}/endereco/{cep} "+cpf+" - "+cep);
+
 		//getDados(cpf)
 		dadosCadastrais.setCpf(cpf);
 		dadosCadastrais.setEndereco(new Endereco((new RestTemplate()).getForObject(
